0ae9eec87b939d9edcd9ed10e75d049f91b48cc2
vaughnpodlogar <vaughn.podlogar@yale.edu>
2018-01-24 10:33:11 -0500

    Added question1

---
+++ Question1.py
@@ -0,0 +1,29 @@
+#x=17
+
+#integer
+
+x = 17
+y1 = x
+print(y1)
+print(type(y1))
+
+#float
+y2 = float(x)
+print(y2)
+print(type(y2))
+
+
+#string
+y3 =str(x)
+print(y3)
+print(type(y3))
+
+#boolean
+y4 = bool(x>5)
+print (y4)
+print(type(y4))
+
+#Type the value of x is 17
+
+text = 'The' + ' ' + 'value' + ' ' + 'x' + ' ' + 'is' + ' ' + y3 + '.'
+print(text)
